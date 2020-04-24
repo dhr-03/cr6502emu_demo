2db624d31d7b0ebc47afabe51fb882552e1406fd
dhr <dhr.git.pub@gmail.com>
2020-04-23 22:17:51 -0300

    demo

---
+++ static/jsm/assembler/snippets/assembler-e79716fcbaa7f3b4/js_snippets/logger.js
@@ -0,0 +1,75 @@
+export class Logger {
+    static baseNode;
+    static workingNode;
+    static writeNode;
+    static currentLine;
+
+    static setup(selector) {
+        this.baseNode = document.querySelector(selector);
+        this.workingNode = null;
+        this.writeNode = null;
+        this.currentLine = -1;
+    }
+
+    static setCurrentLine(line) {
+        this.currentLine = line;
+    }
+
+
+    static beginGeneric(msgClass) {
+        let template = `
+            <div class="message ${msgClass}">
+                <div style="width: 8em;float: left;border-right: 2px solid; margin-right: 1em">
+                    <span>line: ${this.currentLine}</span>
+                </div>
+                <div class="msg-content"></div>
+            </div>
+
+        `;
+
+        let node = document.createElement("div");
+        node.innerHTML = template;
+
+        this.workingNode = node;
+        this.writeNode = node.querySelector(".msg-content");
+
+    }
+
+    static beginInfo() {
+        this.beginGeneric("info");
+    }
+
+    static beginWarn() {
+        this.beginGeneric("warn");
+    }
+
+    static beginErr() {
+        this.beginGeneric("err");
+    }
+
+    static write(obj) {
+        let node =  document.createElement("span");
+
+        node.innerText = " " + String(obj) + " "
+        this.writeNode.appendChild(node);
+    }
+
+    static writeCode(msg) {
+        let node = document.createElement("code");
+        node.innerText = msg;
+
+        this.writeNode.appendChild(node);
+
+    }
+
+    static endMessage() {
+        if (this.workingNode) {
+            this.baseNode.appendChild(this.workingNode);
+
+            this.workingNode= null;
+            this.writeNode = null;
+        }
+    }
+
+
+}
---
+++ static/jsm/assembler/snippets/assembler-e79716fcbaa7f3b4/js_snippets/regex.js
@@ -0,0 +1,48 @@
+const RE_COMMON = String.raw`(\$|b|lo |hi |%|)([A-f0-9]{1,4}|[01]{1,16}|(?:[\+\-]?[0-9]{1,8})|[A-z_][A-z0-9_]{2,10})`;
+
+function execRegex(which, input, bounds) {
+    let results = which.exec(input);
+
+    let arrayI = 0;
+    let findIndexBase = 0;
+    if (results) {
+        for (let i = 1; i < results.length; i++) {
+            let match = results[i];
+
+            if (match === undefined) {
+                bounds[arrayI++] = 0;
+                bounds[arrayI++] = 0;
+            } else {
+                let matchIndex = input.indexOf(match, findIndexBase);
+                findIndexBase = matchIndex;
+
+                bounds[arrayI++] = matchIndex;
+                bounds[arrayI++] = matchIndex + match.length;
+            }
+        }
+    }
+
+    return bounds;
+}
+
+const RE_NORMAL_ADDR = new RegExp(
+    String.raw`^(#?)` +
+    RE_COMMON +
+    String.raw`$`
+);
+
+export function reNormalAddressing(line, container) {
+    return execRegex(RE_NORMAL_ADDR, line, container);
+}
+
+
+const RE_INDEXED_ADDR = new RegExp(
+    String.raw`^(\(|\*|)` +
+    RE_COMMON +
+    String.raw`(\)?),?([XY]?)(\)?)$`
+);
+
+export function reIndexedAddressing(line, container) {
+    return execRegex(RE_INDEXED_ADDR, line, container);
+}
+
